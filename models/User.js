099bcac0a15cdbe1ed526d1c456a2dbb95887fbf
bluewaitor <405622394@qq.com>
2016-12-16 21:03:12 +0800

    添加user保存前的密码加密

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 
 var userSchema = mongoose.Schema({
     username: {
@@ -9,6 +10,31 @@ var userSchema = mongoose.Schema({
         type: String,
         required: true
     }
+}, {timestamps: true});
+
+userSchema.pre('save', function(next) {
+    const user = this;
+    if(!user.isModified('password')) {
+        return next();
+    }
+    bcrypt.genSalt(10, function(err, salt) {
+        if(err) {
+            return next(err);
+        }
+        bcrypt.hash(user.password, salt, null, function(err, hash) {
+            if(err) {
+                return next(err);
+            }
+            user.password = hash;
+            next();
+        });
+    });
 });
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        cb(err, isMatch);
+    });
+};
 
 module.exports = mongoose.model('User', userSchema);
