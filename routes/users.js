d05c09d9203c83794ae654e9c800f70cbc6011bf
bluewaitor <405622394@qq.com>
2017-03-15 15:37:57 +0800

    注册的时候添加判断

--- routes/users.js
+++ routes/users.js
@@ -5,9 +5,30 @@ var User = require('../models/User');
 
 module.exports = {
     signup: function(req, res) {
+        var username = req.body.username;
+        var password = req.body.password;
+        if(!username) {
+            return res.json({
+                success: false,
+                message: "用户名不能为空"
+            });
+        }
+        if(!password) {
+            return res.json({
+                success: false,
+                message: "密码不能为空"
+            });
+        }
+        if(password.length < 6) {
+            return res.json({
+                success: false,
+                message: "密码至少6位"
+            });
+        }
         var user = new User();
-        user.username = req.body.username;
-        user.password = req.body.password;
+        user.username = username;
+        user.password = password;
+        
         user.save(function(err, insertedUser) {
             if(err) {
                 if(err.code == 11000 && err.name == "MongoError") {
