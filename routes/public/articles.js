cea6cb968fb0ceb0b3d1a1a5d5251a7f725d37b6
bluewaitor <405622394@qq.com>
2017-06-19 23:23:30 +0800

    根据文章获取ID评论排序

--- routes/public/articles.js
+++ routes/public/articles.js
@@ -20,7 +20,8 @@ module.exports = {
     },
     getArticleById: function(req, res) {
         var id = req.params.id;
-        Article.findById(id).populate([{path: 'user', select: 'username'}, {path: 'comments'}]).exec(function(err, article) {
+        Article.findById(id).populate([{path: 'user', select: 'username'}, 
+                                {path: 'comments', options: { sort: '-created' }, populate: [{path: 'user', select: 'username'}, {path: 'replyTo', select: 'username'}] }]).exec(function(err, article) {
             if(!err) {
                 return res.json({
                     message: "获取文章详情成功",
