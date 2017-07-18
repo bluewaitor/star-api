186c20cbb30241ab912f175555fd12f683b468b4
bluewaitor <405622394@qq.com>
2017-07-18 23:16:24 +0800

    todo添加用户的判断

--- controllers/todo.js
+++ controllers/todo.js
@@ -29,7 +29,8 @@ module.exports = {
         const page = Number(req.query.page) || 1;
         const limit = Number(req.query.limit) || 10;
         const status = Number(req.query.status) || 0;
-        let todos = await Todo.paginate({status: status}, {page: page, limit: limit, sort: '-created'});
+        const userId = req.decoded.id;
+        let todos = await Todo.paginate({user: userId, status: status}, {page: page, limit: limit, sort: '-created'});
         if (todos) {
             return res.json({
                 success: true,
@@ -42,28 +43,48 @@ module.exports = {
     updateTodoStatus: async (req, res, next) => {
         const {id} = req.params;
         let {status} = req.body;
-        let todo = await Todo.findByIdAndUpdate(id, {status: status}, {new: true});
-        return res.json({
-            success: true,
-            message: '修改状态成功',
-            todo: todo
-        })
+        const userId = req.decoded.id;
+
+        let todo = await Todo.findOneAndUpdate({_id: id, user: userId}, {status: status}, {new: true});
+        if (todo) {
+            return res.json({
+                success: true,
+                message: '修改状态成功',
+                todo: todo
+            })
+        } else {
+            return res.json({
+                success: false,
+                message: '你不是该待办的所有者'
+            })
+        }
+
     },
 
     updateTodoThing: async (req, res, next) => {
         const {id} = req.params;
         let {thing} = req.body;
+        const userId = req.decoded.id;
+
         if (!thing) {
             return res.json({
                 success: false,
                 message: "待办不能为空"
             })
         }
-        let todo = await Todo.findByIdAndUpdate(id, {thing: thing}, {new: true});
-        return res.json({
-            success: true,
-            message: '修改待办成功',
-            todo: todo
-        })
+        let todo = await Todo.findOneAndUpdate({_id: id, user: userId}, {thing: thing}, {new: true});
+        if (todo) {
+            return res.json({
+                success: true,
+                message: '修改待办成功',
+                todo: todo
+            })
+        } else {
+            return res.json({
+                success: false,
+                message: '你不是该待办的所有者'
+            })
+        }
+
     }
 };
